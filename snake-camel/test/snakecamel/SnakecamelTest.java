5d057900fe9e8746dc1c4589b1833e28ed8ef61d
g15937sa <g15937sa@s30444.tsuda.ac.jp>
2017-04-28 11:57:50 +0900

    課題3-2です

---
+++ snake-camel/test/snakecamel/SnakecamelTest.java
@@ -0,0 +1,14 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakecamelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
