6cc1179ab170d6e788f107b7e908bfa707860b16
sakamotoaya <sakamotoaya@192.168.0.6>
2017-05-03 14:26:01 +0900

    課題3-3 エラー修正しました。

--- snake-camel/test/snakecamel/SnakecamelTest.java
+++ snake-camel/test/snakecamel/SnakecamelTest.java
@@ -1,14 +1,36 @@
 package snakecamel;
 
-import static org.junit.Assert.*;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SnakecamelTest {
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeToCamelcaseでapple_orangeをキャメルケースに変換できる(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "AppleOrange";
+		String actual = scu.snakeToCamelcase("apple_orange");
+		assertEquals(expected, actual);
 	}
-
+	@Test
+	public void snakeToCamelcaseでpeach_berryをキャメルケースに変換できる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "PeachBerry";
+		String actual = scu.snakeToCamelcase("peach_berry");
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void camelToSnakecaseでAppleOrangeをスネークケースに変換できる(){
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "apple_orange";
+		String actual = scu.camelToSnakecase("AppleOrange");
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void camelToSnakecaseでPeachBerryをスネークケースに変換できる() {
+		SnakeCamelUtil scu = new SnakeCamelUtil();
+		String expected = "peach_berry";
+		String actual = scu.camelToSnakecase("PeachBerry");
+		assertEquals(expected, actual);
+	}
 }
